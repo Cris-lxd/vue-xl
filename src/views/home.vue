24ea84e193f212d8f36547cffe8cd081fa689f34
Cris114 <15234023889@163.com>
2021-06-29 17:11:39 +0800

    增加退出功能

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,6 +2,20 @@
   <div class="home_container">
       <navBar></navBar>
       <router-view></router-view>
+      <div class="log_out">
+        <Dropdown>
+<!--          <Button type="primary">-->
+<!--            退出登陆-->
+<!--            <Icon type="ios-arrow-down"></Icon>-->
+<!--          </Button>-->
+          <Avatar size="large" src="https://i.loli.net/2017/08/21/599a521472424.jpg" />
+
+          <DropdownMenu slot="list">
+            <DropdownItem @click.native="alertinfo">个人信息</DropdownItem>
+            <DropdownItem @click.native="logout">登出</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
   </div>
 </template>
 
@@ -12,6 +26,15 @@ export default {
     navBar
   },
   methods: {
+    logout() {
+      this.$router.push('/login');
+      sessionStorage.removeItem("user", null);
+      sessionStorage.removeItem("token", null);
+      this.$Message.success("退出登陆成功");
+    },
+    alertinfo() {
+      alert("hahah");
+    }
   }
 }
 </script>
@@ -20,11 +43,23 @@ export default {
 .home_container{
   display: flex;
   padding: 10px;
-  background: green;
+  /*background: green;*/
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
+
 }
+.home_container .log_out{
+  width: 2%;
+  height: 4%;
+  position: absolute;
+  margin-right: 1%;
+  margin-top: 0.5%;
+  right: 0;
+  top: 0;
+  /*background-color: teal;*/
+}
+
 </style>
